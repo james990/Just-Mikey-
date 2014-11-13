9c53ac42bf53f4b0476e2912aeeb2d5782fe2225
james990 <jamescosgrave1234@gmail.com>
2014-11-13 14:12:32 +0000

    Music Artist Website
    This Site was in the development stage for a music artist, the artist
    never got back to me about the site so it is un finished, the site has a
    nice design and makes great use of the jQuery Functions.

---
+++ functions.js
@@ -0,0 +1,111 @@
+
+$(function(){
+$("#home").hide();
+$('#box1').click(function(){
+$('#events').slideUp(800);
+$('#bio').slideUp(800);
+$('#forum').slideUp(800);
+$('#contact').slideUp(800);
+$('#yipyap').slideUp(800);
+$('#home').slideToggle(800);
+
+	});
+	});
+	
+	$(function(){
+$("#events").hide();
+$('#box2').click(function(){
+$('#home').slideUp(800);
+$('bio').slideUp(800);
+$('#forum').slideUp(800);
+$('#contact').slideUp(800);
+$('#yipyap').slideUp(800);
+$('#events').slideToggle(800);
+
+	});
+	});
+	
+	$(function(){
+$("#bio").hide();
+$('#box3').click(function(){
+$('#home').slideUp(800);
+$('#events').slideUp(800);
+$('#forum').slideUp(800);
+$('#contact').slideUp(800);
+$('#yipyap').slideUp(800);
+$('#bio').slideToggle(800);
+	});
+	});
+	
+	$(function(){
+$("#forum").hide();
+$('#box4').click(function(){
+$('#bio').slideUp(800);
+$('#events').slideUp(800);
+$('#home').slideUp(800);
+$('#contact').slideUp(800);
+$('#yipyap').slideUp(800);
+$('#forum').slideToggle(800);
+	});
+	});
+	
+		$(function(){
+$("#contact").hide();
+$('#box5').click(function(){
+$('#forum').slideUp(800);
+$('#events').slideUp(800);
+$('#bio').slideUp(800);
+$('#home').slideUp(800);
+$('#yipyap').slideUp(800);
+$('#contact').slideToggle(800);
+	});
+	});
+	
+		$(function(){
+$("#yipyap").hide();
+$('#box6').click(function(){
+$('#contact').slideUp(800);
+$('#events').slideUp(800);
+$('#bio').slideUp(800);
+$('#forum').slideUp(800);
+$('#home').slideUp(800);
+$('#yipyap').slideToggle(800);
+	});
+	});
+	
+		$(function(){
+$('#close').click(function(){
+$('#home').slideUp(800);
+	});
+	});
+
+		$(function(){
+$('#close2').click(function(){
+$('#events').slideUp(800);
+
+	});
+	});
+		$(function(){
+$('#close3').click(function(){
+$('#bio').slideUp(800);
+
+	});
+	});
+		$(function(){
+$('#close4').click(function(){
+$('#forum').slideUp(800);
+
+	});
+	});
+		$(function(){
+$('#close5').click(function(){
+$('#contact').slideUp(800);
+
+	});
+	});
+		$(function(){
+$('#close6').click(function(){
+$('#yipyap').slideUp(800);
+
+	});
+	});
